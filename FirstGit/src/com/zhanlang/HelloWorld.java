cc5f06d2c4ce04394073819717fd0cf88978f294
HaKuNa <734216267@qq.com>
2019-09-24 23:07:44 +0800

    第一次提交FirstGit整个项目

---
+++ FirstGit/src/com/zhanlang/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zhanlang;
+
+/**
+ * Created by HaKuNa on 2019/9/23.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello, World!");
+        System.out.print("Hello, Git333!");
+    }
+}
